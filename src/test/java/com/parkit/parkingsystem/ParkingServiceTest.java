a0ad201e94ab92f290284fa43960e28b812a0d2a
divineion <najima.guermoudi@gmail.com>
2025-02-28 23:54:37 +0100

    src\test\java\com\parkit\parkingsystem\ParkingServiceTest.java :
    Update existing unit tests and implement new tests.
    
    To avoid errors in cases where the stub is not attempted :
      - move the vehicle registration mock setup into a separate useVehicleRegistrationNumber() method ;
      - make the ticket creation static.
    
     - add a new helper method useStubsToGetAndUpdateTicketForExitTests() to centralize mocks for exit-related tests.
    
     - reset the System.out stream in @BeforeEach to capture output during tests and restore it to the standard output in @AfterEach for clean test execution.
    
     - replace manual instantiation of ParkingService with the @InjectMocks annotation.
    
     - add testProcessIncomingVehicle() to check handling of a vehicle entering the parking lot.
    
    - add testProcessIncomingVehicleForFrequentUser() to check the loyalty message display.
    
    - add processExitingVehicleTest() to check the vehicle exit process and parking update.

--- src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
@@ -7,20 +7,31 @@ import com.parkit.parkingsystem.model.ParkingSpot;
 import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Date;
-
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
+
+import java.util.Date;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingServiceTest {
 
+    @InjectMocks
     private static ParkingService parkingService;
 
     @Mock
@@ -30,32 +41,94 @@ public class ParkingServiceTest {
     @Mock
     private static TicketDAO ticketDAO;
 
-    @BeforeEach
-    private void setUpPerTest() {
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    private final PrintStream standardOut = System.out;
+
+    private static Ticket ticket = new Ticket();
+
+    private void useVehicleRegistrationNumber() {
         try {
             when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to set up test mock objects");
+        }
+    }
 
+    private void useStubsToGetAndUpdateTicketForExitTests() {
+        useVehicleRegistrationNumber();
+        when(ticketDAO.getTicket(anyString())).thenReturn(ticket);
+        ticket.setOutTime(new Date(System.currentTimeMillis()));
+        when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(true);
+    }
+
+
+    @BeforeEach
+    private void setUpPerTest() {
+        outputStreamCaptor.reset();
+        System.setOut(new PrintStream(outputStreamCaptor));
+
+        try {
             ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false);
-            Ticket ticket = new Ticket();
             ticket.setInTime(new Date(System.currentTimeMillis() - (60*60*1000)));
             ticket.setParkingSpot(parkingSpot);
             ticket.setVehicleRegNumber("ABCDEF");
-            when(ticketDAO.getTicket(anyString())).thenReturn(ticket);
-            when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(true);
-
-            when(parkingSpotDAO.updateParking(any(ParkingSpot.class))).thenReturn(true);
-
-            parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         } catch (Exception e) {
             e.printStackTrace();
             throw  new RuntimeException("Failed to set up test mock objects");
         }
     }
 
-    @Test
-    public void processExitingVehicleTest(){
-        parkingService.processExitingVehicle();
-        verify(parkingSpotDAO, Mockito.times(1)).updateParking(any(ParkingSpot.class));
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
     }
 
+    @Test
+    public void testProcessIncomingVehicle() {
+        useVehicleRegistrationNumber();
+        when(inputReaderUtil.readSelection()).thenReturn(1);
+        when(parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR)).thenReturn(1);
+        parkingService.processIncomingVehicle();
+
+        verify(ticketDAO, Mockito.times(1)).getNbTicket(anyString());
+        verify(ticketDAO, Mockito.times(1)).isVehicleAlreadyInParking(anyString());
+        verify(parkingSpotDAO, Mockito.times(1)).updateParking(any(ParkingSpot.class));
+
+
+        assertEquals(1, parkingService.getNextParkingNumberIfAvailable().getId());
+    }
+
+    @Test
+    public void testProcessIncomingVehicle_WithLoyaltyDiscountMessage() {
+        useVehicleRegistrationNumber();
+        when(inputReaderUtil.readSelection()).thenReturn(1);
+        when(parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR)).thenReturn(1);
+        when(ticketDAO.getNbTicket(ticket.getVehicleRegNumber())).thenReturn(5);
+
+        parkingService.processIncomingVehicle();
+
+        String output = outputStreamCaptor.toString().trim();
+
+        String expectedOutput = "Welcome back! As a regular user of our parking, you will receive a 5% discount.";
+        assertTrue(output.contains(expectedOutput));
+    }
+
+    @Test
+    public void processExitingVehicleTest(){
+        useVehicleRegistrationNumber();
+        useStubsToGetAndUpdateTicketForExitTests();
+        when(parkingSpotDAO.updateParking(any(ParkingSpot.class))).thenReturn(true);
+
+        parkingService.processExitingVehicle();
+        verify(parkingSpotDAO, Mockito.times(1)).updateParking(any(ParkingSpot.class));
+        verify(ticketDAO).getTicket(anyString());
+        verify(ticketDAO).getNbTicket(anyString());
+        verify(ticketDAO).updateTicket(ticket);
+        verify(parkingSpotDAO).updateParking(any(ParkingSpot.class));
+
+        String output = outputStreamCaptor.toString().trim();
+        String expectedOutput = "Please pay the parking fare:";
+        assertTrue(output.contains(expectedOutput));
+    }
 }
