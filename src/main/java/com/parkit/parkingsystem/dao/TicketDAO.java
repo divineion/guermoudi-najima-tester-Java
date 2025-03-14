e5801308e55ca6da14e6307abf88d9b950e70fd2
divineion <najima.guermoudi@gmail.com>
2025-03-14 11:11:39 +0100

    src\main\java\com\parkit\parkingsystem\dao\TicketDAO.java
    Refactor TicketDAO: Replace Date with Calendar for handling timestamps ;
    Add a condition to check if the outTime ticket is null in getTicket() method ;
    
    Format code.

--- src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
+++ src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
@@ -12,6 +12,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Timestamp;
+import java.util.Calendar;
 
 public class TicketDAO {
 
@@ -30,8 +31,8 @@ public class TicketDAO {
             ps.setInt(1,ticket.getParkingSpot().getId());
             ps.setString(2, ticket.getVehicleRegNumber());
             ps.setDouble(3, ticket.getPrice());
-            ps.setTimestamp(4, new Timestamp(ticket.getInTime().getTime()));
-            ps.setTimestamp(5, (ticket.getOutTime() == null)?null: (new Timestamp(ticket.getOutTime().getTime())) );
+            ps.setTimestamp(4, new Timestamp(ticket.getInTime().getTimeInMillis()));
+            ps.setTimestamp(5, (ticket.getOutTime() == null) ? null : (new Timestamp(ticket.getOutTime().getTimeInMillis())) );
             result = ps.execute();
         }catch (Exception e){
             logger.error("Error fetching next available slot",e);
@@ -52,13 +53,24 @@ public class TicketDAO {
             ResultSet rs = ps.executeQuery();
             if(rs.next()){
                 ticket = new Ticket();
+                Calendar inTime = Calendar.getInstance();
                 ParkingSpot parkingSpot = new ParkingSpot(rs.getInt(1), ParkingType.valueOf(rs.getString(6)),false);
+                
                 ticket.setParkingSpot(parkingSpot);
                 ticket.setId(rs.getInt(2));
                 ticket.setVehicleRegNumber(vehicleRegNumber);
                 ticket.setPrice(rs.getDouble(3));
-                ticket.setInTime(rs.getTimestamp(4));
-                ticket.setOutTime(rs.getTimestamp(5));
+                inTime.setTimeInMillis(rs.getTimestamp(4).getTime());
+                ticket.setInTime(inTime);              
+                Timestamp outTime = rs.getTimestamp(5);
+
+                if (outTime != null) {
+                    Calendar outCalendar = Calendar.getInstance();
+                    outCalendar.setTime(outTime);
+                    ticket.setOutTime(outCalendar);
+                } else {
+                    ticket.setOutTime(null);
+                }
             }
             dataBaseConfig.closeResultSet(rs);
             dataBaseConfig.closePreparedStatement(ps);
@@ -67,24 +79,30 @@ public class TicketDAO {
         }finally {
             dataBaseConfig.closeConnection(con);
         }
-            return ticket;
+            
+        return ticket;
     }
 
     public boolean updateTicket(Ticket ticket) {
         Connection con = null;
         try {
             con = dataBaseConfig.getConnection();
+    
             PreparedStatement ps = con.prepareStatement(DBConstants.UPDATE_TICKET);
             ps.setDouble(1, ticket.getPrice());
-            ps.setTimestamp(2, new Timestamp(ticket.getOutTime().getTime()));
+            ps.setTimestamp(2, new Timestamp(ticket.getOutTime().getTimeInMillis()));
             ps.setInt(3,ticket.getId());
-            ps.execute();
+            ps.executeUpdate();
+            
+            dataBaseConfig.closeConnection(con);
+
             return true;
         }catch (Exception e){
             logger.error("Error saving ticket info", e);
         }finally {
             dataBaseConfig.closeConnection(con);
         }
+
         return false;
     }
 
@@ -115,10 +133,10 @@ public class TicketDAO {
 
         } catch(Exception e) {
             logger.error("An error occurred : ", e);
-        }
-        finally {
+        } finally {
             dataBaseConfig.closeConnection(con);    
         }
+
         return isInParking;
     }
 
