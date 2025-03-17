5a8aa6f33d555200fb863c7602887403936c51e2
divineion <najima.guermoudi@gmail.com>
2025-03-17 11:07:50 +0100

    src\main\java\com\parkit\parkingsystem\dao\ParkingSpotDAO.java :
    Refactor exception handling :
     - remove unnecessary finally blocks to improve precision in exception handling ;
     - replaced with try-with-resources for automatic resource management, wrap the if block in a try-with-resources to close the ResultSet automatically.

--- src/main/java/com/parkit/parkingsystem/dao/ParkingSpotDAO.java
+++ src/main/java/com/parkit/parkingsystem/dao/ParkingSpotDAO.java
@@ -10,49 +10,43 @@ import org.apache.logging.log4j.Logger;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class ParkingSpotDAO {
     private static final Logger logger = LogManager.getLogger("ParkingSpotDAO");
 
     public DataBaseConfig dataBaseConfig = new DataBaseConfig();
 
-    public int getNextAvailableSlot(ParkingType parkingType){
-        Connection con = null;
-        int result=-1;
-        try {
-            con = dataBaseConfig.getConnection();
-            PreparedStatement ps = con.prepareStatement(DBConstants.GET_NEXT_PARKING_SPOT);
+    public int getNextAvailableSlot(ParkingType parkingType) {
+        int result = -1;
+        try (Connection con = dataBaseConfig.getConnection();
+                PreparedStatement ps = con.prepareStatement(DBConstants.GET_NEXT_PARKING_SPOT)) {
             ps.setString(1, parkingType.toString());
-            ResultSet rs = ps.executeQuery();
-            if(rs.next()){
-                result = rs.getInt(1);;
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    result = rs.getInt(1);
+                    ;
+                }
             }
-            dataBaseConfig.closeResultSet(rs);
-            dataBaseConfig.closePreparedStatement(ps);
-        }catch (Exception ex){
-            logger.error("Error fetching next available slot",ex);
-        }finally {
-            dataBaseConfig.closeConnection(con);
+
+        } catch (SQLException | ClassNotFoundException ex) {
+            logger.error("Error fetching next available slot", ex);
         }
+
         return result;
     }
 
-    public boolean updateParking(ParkingSpot parkingSpot){
-        //update the availability fo that parking slot
-        Connection con = null;
-        try {
-            con = dataBaseConfig.getConnection();
-            PreparedStatement ps = con.prepareStatement(DBConstants.UPDATE_PARKING_SPOT);
+    public boolean updateParking(ParkingSpot parkingSpot) {
+        // update the availability fo that parking slot
+        try (Connection con = dataBaseConfig.getConnection();
+                PreparedStatement ps = con.prepareStatement(DBConstants.UPDATE_PARKING_SPOT)) {
             ps.setBoolean(1, parkingSpot.isAvailable());
             ps.setInt(2, parkingSpot.getId());
             int updateRowCount = ps.executeUpdate();
-            dataBaseConfig.closePreparedStatement(ps);
             return (updateRowCount == 1);
-        }catch (Exception ex){
-            logger.error("Error updating parking info",ex);
+        } catch (SQLException | ClassNotFoundException ex) {
+            logger.error("Error updating parking info", ex);
             return false;
-        }finally {
-            dataBaseConfig.closeConnection(con);
         }
     }
 
